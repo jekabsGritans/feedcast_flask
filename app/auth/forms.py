ab015c4ad5f6ea69b3d9ffedcc7f222a0d00938f
Jekabs <jekabs@gritans.lv>
2022-03-18 22:22:15 +0200

    render wtforms from calls

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,3 +5,6 @@ from wtforms.validators import Required, Email, EqualTo
 class LoginForm(Form):
     email = TextField('Email Address', [Email(), Required(message='Enter your email address.')])
     password = PasswordField('Password', [Required(message = 'Enter your password.')])
+
+class SignUp(Form):
+    ...
