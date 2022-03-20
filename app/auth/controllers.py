71647969e12b089226e7e5807520f89b92d65270
Jekabs <jekabs@gritans.lv>
2022-03-20 18:57:31 +0000

    added signup

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -1,26 +1,40 @@
 from flask import Blueprint, request, render_template, flash, session, redirect, url_for
-from werkzeug import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from app import db
-from app.auth.forms import LoginForm
+from app.auth.forms import SignInForm, SignUpForm
 from app.auth.models import User
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth.route('/signin/', methods=['POST','GET'])
 def signin():
-    form = LoginForm(request.form)
+    form = SignInForm(request.form)
     if form.validate_on_submit():
+        # if request.form['submit'] == 'sign-up':
+        #     return redirect(url_for('auth.signup'))
         user = User.query.filter_by(email=form.email.data).first() 
         if user and check_password_hash(user.password, form.password.data):
             session['user'] = user.id
-            flash(f'Hello {user.name}!')
             if 'url' in session:
                 return redirect(session['url'])
             else:
-                return redirect() 
-        flash('Incorrect email or password', 'error-message')
+                return redirect()
+        flash('Incorrect email or password')
     return render_template('auth/signin.html',form=form)
 
+@auth.route('/signup/', methods=['POST','GET'])
+def signup():
+    form = SignUpForm(request.form)
+    if form.validate_on_submit():
+        user = User()
+        form.populate_obj(user)
+        # db.session.add(user)
+        if 'url' in session:
+            return redirect(session['url'])
+        else:
+            return redirect()
+    return render_template('auth/signup.html', form=form)
+        
 @auth.route('/singout/')
 def signout():
     if 'user' in session:
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,13 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import Required, Email, EqualTo
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, PasswordField, StringField
+from wtforms.validators import InputRequired, Email, EqualTo
 
-class LoginForm(Form):
-    email = TextField('Email Address', [Email(), Required(message='Enter your email address.')])
-    password = PasswordField('Password', [Required(message = 'Enter your password.')])
+class SignInForm(FlaskForm):
+    email = StringField('Email Address', [Email(), InputRequired(message='Enter your email address.')])
+    password = PasswordField('Password', [InputRequired(message = 'Enter your password.')])
 
-class SignUp(Form):
-    ...
+class SignUpForm(FlaskForm):
+    email = StringField('Email Address', [Email(), InputRequired(message='Enter your email address.')])
+    name = StringField('Name', [InputRequired(message='Enter your name.')])
+    password = PasswordField('Password', [InputRequired(message = 'Enter your password.')])
+    confirm_password = PasswordField('Password', [EqualTo(password, 'Passwords must match.')])
