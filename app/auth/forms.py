bc55cac18a6948f0db7d7c6b27c524a58e7c8419
Jekabs <jekabs@gritans.lv>
2022-05-21 11:40:47 +0300

    a tag redirects for signin/up

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, PasswordField, StringField
+from wtforms import TextAreaField, PasswordField, StringField, SubmitField
 from wtforms.validators import InputRequired, Email, EqualTo
+
 
 class SignInForm(FlaskForm):
     email = StringField('Email Address') #, [Email(), InputRequired(message='Enter your email address.')])
     password = PasswordField('Password')#, [InputRequired(message = 'Enter your password.')])
+    submit = SubmitField('Sign In')
 
 class SignUpForm(FlaskForm):
     email = StringField('Email Address', [Email(), InputRequired(message='Enter your email address.')])
     name = StringField('Name', [InputRequired(message='Enter your name.')])
     password = PasswordField('Password', [InputRequired(message = 'Enter your password.')])
     confirm_password = PasswordField('Confirm Password', [EqualTo(password, 'Passwords must match.')])
+    submit = SubmitField('Sign Up')
